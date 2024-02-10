4d751ead766d58bb5ca210dba97fe331803f324c
Oualid CHAOUATI <95523516+Oualid-CHAOUATI@users.noreply.github.com>
2024-02-10 04:05:42 +0100

    set custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Cheat } from "./components/Cheat/Cheat";
 
 import "./App.css";
 import { BG } from "./UI/BG";
-import { ProgessBar } from "./UI/ProgessBar/ProgessBar";
 export const App = () => {
   return (
     <Flex isColumn gap="2em">
